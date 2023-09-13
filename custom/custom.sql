74073bb070010f3edefab2957cc1fb264748711f
Tommaso <Masomattei@gmail.com>
2023-09-13 16:35:52 +0200

    Update custom.sql

--- custom/custom.sql
+++ custom/custom.sql
@@ -1,6 +1,7 @@
 CREATE TABLE commissione( --TABELLA RELATIVA ALLA COMMISSIONE DI MAGLIETTE PERSONALIZZATE
     immagine varchar(100) not null,
     decorazione varchar(100) not null,
+    utente varchar(30) not null,
     indirizzo varchar(30),
     telefono Integer,
     descrizione varchar(297) not null,
